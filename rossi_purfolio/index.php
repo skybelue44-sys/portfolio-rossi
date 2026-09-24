14c2c785c31fe6d7d104a30f9d28f8a633d009ad
skybelue44-sys <skybelue44@gmail.com>
2026-09-24 10:14:32 +0700

    Add files via upload

---
+++ rossi_purfolio/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROSSI FEBRIYANTO</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+      <a class="navbar-brand" href="#">rossi</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"  aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+           <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+           </li>
+               <li class="nav-item">
+               <a class="nav-link" href="#">About</a>
+               </li>
+                   <li class="nav-item">
+                   <a class="nav-link" href="#">Projek</a>
+                   </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Kontak</a>
+                        </li>
+                           </ul>
+          <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+</nav>
+                         <section class="jumbotron text-center" style="background-color:azure;">
+                         <img src="alanuy.jpg" alt="" width="200px" class="rounded-circle">
+                         <h1 class="display-4">Rossi</h1>
+                         <p class="lead">Budak SMK MUMA</p>
+                         </section>
+
+
+                         <section id="about">
+                            <div class="container">
+                                <div class="row text-center">
+                                    <div class="col">
+                                        <h2>ABOUT</h2>
+                                         </div>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col">
+                                             <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sed officia adipisci excepturi recusandae culpa doloremque deserunt eos perspiciatis debitis praesentium, temporibus mollitia, repudiandae illo impedit ea eum iste! Ex repellendus sed soluta, facilis modi tempore quibusdam veritatis commodi at hic! Enim modi eius facilis quos? Aliquid dolor temporibus numquam vitae quidem libero, molestiae eius laborum sed rerum voluptatem illo quisquam accusamus possimus esse illum. Soluta sint quod consequatur, error quae iusto exercitationem officia velit assumenda nostrum possimus vero facilis eveniet, molestias temporibus modi ducimus libero tenetur sed ipsam a saepe aliquam. Praesentium, dicta reiciendis aliquam minus laborum aliquid optio?
+                                             </p>
+                                            </div>
+                                            <div class="col">
+                                               <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem harum nobis eaque beatae vitae maxime esse quaerat quas numquam temporibus.
+                                               </p>
+                                            </div>
+                                         </div>
+                                         </div>
+                                           </sectoon>
+</body>
+</html>
